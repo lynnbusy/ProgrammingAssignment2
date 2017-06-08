9180ab8068956023506a534f16a8cf24032be8c1
Xiao Yu <lynnbusy@gmail.com>
2017-06-09 02:15:07 +0800

    Finish the assignment!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
 ## Write a short comment describing this function
+## This program use data caching to optimize matrix inversion. 
 
+## create a cachedMatrix with bound matrix "x", inversion result "m"
+## and two pairs of getter/setter.
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
+## get the solved result of cachedMatrix "x"
+## if the result has been computed, return the cached result.
+## else, compute the result and set it back for caching.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
